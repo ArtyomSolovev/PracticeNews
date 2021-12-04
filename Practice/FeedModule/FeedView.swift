fa3a1b574e34c9aa00c551fbd1aaa9daac5f0234
ArtyomSolovev <artyom.solovev@icloud.com>
2021-12-04 15:31:20 +0300

    Some correct

--- Practice/FeedModule/FeedView.swift
+++ Practice/FeedModule/FeedView.swift
@@ -13,7 +13,6 @@ final class FeedView: UIView {
     
     lazy var tableView: UITableView = {
         let view = UITableView(frame: .zero, style: .grouped)
-        view.backgroundColor = .gray
         view.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.id)
         view.translatesAutoresizingMaskIntoConstraints = false
         view.rowHeight = 77.0
@@ -38,10 +37,11 @@ final class FeedView: UIView {
     }
     
     func configure() {
+        self
         self.addSubview(tableView)
-        self.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        self.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        self.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        self.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
 }
